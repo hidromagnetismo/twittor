2aa06fa94ca7706bce8a60800cb91725f83d8955
Liscano Natanael <hidromagnetismo@gmail.com>
2021-03-10 03:53:12 +0000

    Endpoint DELETE /eliminarTweet, eliminacion de un tweet de un usuario, solucion por Pablo Tilotta

--- test/api.test.js
+++ test/api.test.js
@@ -1,7 +1,7 @@
 
 // import test from 'jest';
 // import puppeteer from 'puppeteer';
-const { GET, POST, PUT } = require('./src/restClientPuppeteer');
+const { GET, POST, PUT, DELETE } = require('./src/restClientPuppeteer');
 let __URL__, __DB__, Headers, Body, ResponseText, ResponseJSON;
 
 const { exec } = require('child_process');
@@ -217,9 +217,6 @@ async function registerAndLogin() {
     expect(DB_usuario.email).toBe(email);
 
     // Realizamos el login con el usuario previamente registrado/guardado
-    Headers = {
-        'Content-Type': 'application/json'
-    }
     Body = {
         email,
         password
@@ -314,10 +311,6 @@ describe('Endpoint POST /login, login', () => {
         expect(DB_usuario.email).toBe(email);
 
         // Realizamos el login con el usuario previamente registrado/guardado
-        Headers = {
-            'Content-Type': 'application/json'
-        }
-
         const __INCORRECT__ = 'contraseña_incorrecta';
         
         Body = {
@@ -406,8 +399,8 @@ describe('Endpoint GET /verPerfil, perfil del usuario luego de haberse logueado'
         ResponseJSON = JSON.parse(ResponseText);
         
         // Confirmando respuesta
-        expect(ResponseJSON.id).toBeDefined();
-        expect(ResponseJSON.id).toBe(DB_usuario._id.toString());
+        expect(ResponseJSON._id).toBeDefined();
+        expect(ResponseJSON._id).toBe(DB_usuario._id.toString());
         expect(ResponseJSON.email).toBeDefined();
         expect(ResponseJSON.email).toBe(email);
         expect(ResponseJSON.email).toBe(DB_usuario.email);
@@ -485,17 +478,12 @@ describe('Endpoint GET /modificarPerfil, modificando el perfil del usuario', () 
 
 
         // Tayendose el perfil luego de haber actualizado
-        Headers = {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer${ResponseJSON.token}`
-        }
-
         ResponseText = await GET(`${__URL__}/verPerfil?id=${DB_usuario._id}`, Headers);
         ResponseJSON = JSON.parse(ResponseText);
 
         // Confirmando respuesta
-        expect(ResponseJSON.id).toBeDefined();
-        expect(ResponseJSON.id).toBe(DB_usuario_update._id.toString());
+        expect(ResponseJSON._id).toBeDefined();
+        expect(ResponseJSON._id).toBe(DB_usuario_update._id.toString());
 
         expect(ResponseJSON.email).toBeDefined();
         expect(ResponseJSON.email).toBe(email);
@@ -603,29 +591,29 @@ describe('Endpoint POST /tweet, insertar un Tweet', () => {
 
 
 
-//   ,ad8888ba,   88888888888  888888888888                                  
-//  d8"'    `"8b  88                88                                       
-// d8'            88                88                                       
-// 88             88aaaaa           88                                       
-// 88      88888  88"""""           88                                       
-// Y8,        88  88                88                                       
-//  Y8a.    .a88  88                88                                       
-//   `"Y88888P"   88888888888       88                                       
-                                                                          
-                                                                          
-                                                                          
-//           d8                                                              
-//         ,8P'  ,d                                                   ,d     
-//        d8"    88                                                   88     
-//      ,8P'   MM88MMM  8b      db      d8   ,adPPYba,   ,adPPYba,  MM88MMM  
-//     d8"       88     `8b    d88b    d8'  a8P_____88  a8P_____88    88     
-//   ,8P'        88      `8b  d8'`8b  d8'   8PP"""""""  8PP"""""""    88     
-//  d8"          88,      `8bd8'  `8bd8'    "8b,   ,aa  "8b,   ,aa    88,    
-// 8P'           "Y888      YP      YP       `"Ybbd8"'   `"Ybbd8"'    "Y888  
+//   ,ad8888ba,   88888888888  888888888888                                                                       
+//  d8"'    `"8b  88                88                                                                            
+// d8'            88                88                                                                            
+// 88             88aaaaa           88                                                                            
+// 88      88888  88"""""           88                                                                            
+// Y8,        88  88                88                                                                            
+//  Y8a.    .a88  88                88                                                                            
+//   `"Y88888P"   88888888888       88                                                                            
+                                                                                                               
+                                                                                                               
+                                                                                                               
+//           d8  88                      888888888888                                                             
+//         ,8P'  88                           88                                                ,d                
+//        d8"    88                           88                                                88                
+//      ,8P'     88   ,adPPYba,   ,adPPYba,   88  8b      db      d8   ,adPPYba,   ,adPPYba,  MM88MMM  ,adPPYba,  
+//     d8"       88  a8P_____88  a8"     "8a  88  `8b    d88b    d8'  a8P_____88  a8P_____88    88     I8[    ""  
+//   ,8P'        88  8PP"""""""  8b       d8  88   `8b  d8'`8b  d8'   8PP"""""""  8PP"""""""    88      `"Y8ba,   
+//  d8"          88  "8b,   ,aa  "8a,   ,a8"  88    `8bd8'  `8bd8'    "8b,   ,aa  "8b,   ,aa    88,    aa    ]8I  
+// 8P'           88   `"Ybbd8"'   `"YbbdP"'   88      YP      YP       `"Ybbd8"'   `"Ybbd8"'    "Y888  `"YbbdP"'  
 
 //      .only
 //      .skip
-describe('Endpoint GET /tweet, leer los tweets de un usuario', () => {
+describe('Endpoint GET /leoTweets, leer los tweets de un usuario', () => {
     
     //.only
     //.skip
@@ -732,6 +720,81 @@ describe('Endpoint GET /tweet, leer los tweets de un usuario', () => {
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+// 88888888ba,    88888888888  88           88888888888  888888888888  88888888888                                                                     
+// 88      `"8b   88           88           88                88       88                                                                              
+// 88        `8b  88           88           88                88       88                                                                              
+// 88         88  88aaaaa      88           88aaaaa           88       88aaaaa                                                                         
+// 88         88  88"""""      88           88"""""           88       88"""""                                                                         
+// 88         8P  88           88           88                88       88                                                                              
+// 88      .a8P   88           88           88                88       88                                                                              
+// 88888888Y"'    88888888888  88888888888  88888888888       88       88888888888                                                                     
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+//           d8          88  88                      88                                  888888888888                                                  
+//         ,8P'          88  ""                      ""                                       88                                                ,d     
+//        d8"            88                                                                   88                                                88     
+//      ,8P'  ,adPPYba,  88  88  88,dPYba,,adPYba,   88  8b,dPPYba,   ,adPPYYba,  8b,dPPYba,  88  8b      db      d8   ,adPPYba,   ,adPPYba,  MM88MMM  
+//     d8"   a8P_____88  88  88  88P'   "88"    "8a  88  88P'   `"8a  ""     `Y8  88P'   "Y8  88  `8b    d88b    d8'  a8P_____88  a8P_____88    88     
+//   ,8P'    8PP"""""""  88  88  88      88      88  88  88       88  ,adPPPPP88  88          88   `8b  d8'`8b  d8'   8PP"""""""  8PP"""""""    88     
+//  d8"      "8b,   ,aa  88  88  88      88      88  88  88       88  88,    ,88  88          88    `8bd8'  `8bd8'    "8b,   ,aa  "8b,   ,aa    88,    
+// 8P'        `"Ybbd8"'  88  88  88      88      88  88  88       88  `"8bbdP"Y8  88          88      YP      YP       `"Ybbd8"'   `"Ybbd8"'    "Y888  
+
+//      .only
+//      .skip
+describe('Endpoint DELETE /eliminarTweet, eliminar un tweet', () => {
+    //.only
+    //.skip
+    it('Default', async () => {
+
+        // Primero creamos el Tweet a que luego se va a eliminar
+
+        // Creamos el usuario y nos logueamos
+        let {email, DB_usuario, ResponseJSON} = await registerAndLogin();
+
+        // Creando el tweet
+        Headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer${ResponseJSON.token}`
+        }
+        const date = parseInt(Date.now());
+        const mensaje = `Es es mi primer tweet ${date}`;
+        Body = {
+            mensaje
+        }
+        ResponseText = await POST(`${__URL__}/tweet`, Headers, Body);
+
+        let DB_tweet = await (await db()).collection('tweet').findOne({mensaje: mensaje});
+        
+        // Verificando que se haya registrado en la base de datos
+        expect(DB_tweet.mensaje).toBe(mensaje);
+        
+        // Checando el usuario dueño del tweet:
+        expect(DB_tweet.userId).toBeDefined();
+        expect(DB_tweet.userId.toString()).toBe(DB_usuario._id.toString());
+
+
+        // Ahora procedemos a borrar el tweet previamente creado
+        ResponseText = await DELETE(`${__URL__}/eliminarTweet?id=${DB_tweet._id.toString()}`, Headers);
+
+        // Confirmamos que se haya borrado
+        DB_tweet = await (await db()).collection('tweet').findOne({mensaje: mensaje});
+        
+        expect(DB_tweet).toBeNull()
+    });
+
+});
 
 
 
